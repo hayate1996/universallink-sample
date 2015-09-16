cfe44c98e766ce11ad6d8f4fff75a6de8dcfa773
NAGAMINE HIROMASA <hiromasa.nagamine@linecorp.com>
2015-09-16 11:43:03 +0900

    first commit

---
+++ universallink-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  universallink-sample
+//
+//  Created by NAGAMINE HIROMASA on 2015/09/16.
+//  Copyright © 2015年 Hiromasa Nagamine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
